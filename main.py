adeae6d3fb8d4a33cc895d3d690cf7c14416b3ea
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2023-02-04 20:55:51 -0300

    Esse é um commit v0.0.1

--- main.py
+++ main.py
@@ -31,4 +31,5 @@ if __name__ == "__main__":
     # system("taskkill /im chrome.exe /f") # Podemos colocar isso para fechar o chrome antes de rodar o eel
     eel.spawn(start_eel)  # Inicializando eel em outro thread
     while True:
-        eel.sleep( 1 )
+        
+        eel.sleep(1)
